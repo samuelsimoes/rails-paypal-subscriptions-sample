d9f77df1e2d3b9e5cf63d8498485000f0deb1fc5
Samuel Simões <slip.cm@gmail.com>
2014-09-29 20:28:48 -0300

    Created the logic to receive the Paypal notification

--- app/controllers/paypal_controller.rb
+++ app/controllers/paypal_controller.rb
@@ -2,6 +2,27 @@ class PaypalController < ApplicationController
   protect_from_forgery except: [:ipn_listener]
 
   def ipn_listener
+    if params[:txn_type].present?
+      PaypalSubscription::NotificationHandler.resolve!(
+        subscription: subscription,
+        notification: notification
+      )
+    end
+
     render nothing: true
+  end
+
+  private
+
+  def subscription
+    @subscription ||=
+      Subscription.find_by!(
+        id: notification.reference,
+        paypal_payer_id: notification.payer_id
+      )
+  end
+
+  def notification
+    @notification ||= PayPal::Recurring::Notification.new(params)
   end
 end
