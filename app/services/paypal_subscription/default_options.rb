1e46c392f8798972f4f90cb15d034ca89a69b001
Samuel Simões <slip.cm@gmail.com>
2014-09-29 19:50:22 -0300

    Added the Paypal subscription options class

---
+++ app/services/paypal_subscription/default_options.rb
@@ -0,0 +1,18 @@
+class PaypalSubscription::DefaultOptions
+  def self.for(subscriptable)
+    {
+      period: :monthly,
+      outstanding: :no_auto,
+      frequency: 1,
+      start_at: Time.current,
+      trial_length: 0,
+      payer_id: subscriptable.paypal_payer_id,
+      profile_id: subscriptable.paypal_profile_id,
+      reference: subscriptable.id,
+      description: subscriptable.paypal_description,
+      amount: subscriptable.price,
+      currency: 'BRL',
+      locale: 'pt_BR'
+    }
+  end
+end
