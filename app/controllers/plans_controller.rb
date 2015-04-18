021b45fbe668132aa438747f7c00f66c9f6cb396
Samuel Simões <slip.cm@gmail.com>
2015-04-18 12:13:46 -0300

    Simplified the plans controller

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,18 +1,5 @@
 class PlansController < ApplicationController
-  before_action :set_plan, only: [:show, :edit, :update, :destroy]
-
-  # GET /plans
   def index
     @plans = Plan.all
   end
-
-  # GET /plans/1
-  def show
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_plan
-      @plan = Plan.find(params[:id])
-    end
 end
